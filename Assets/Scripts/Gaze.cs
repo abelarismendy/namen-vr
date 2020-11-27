aefdbd3f134f013a63c5ae78940db347a99ac1d7
e-gonzalez5 <e.gonzalez5@uniandes.edu.co>
2020-11-26 19:31:23 -0500

    Script VR gaze

---
+++ Assets/Scripts/Gaze.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Gaze : MonoBehaviour
+{
+    public float gazetime = 2f;
+    private float timer;
+    private bool gazed;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (gazed) {
+            timer += Time.deltaTime;
+
+            if (timer >= gazetime) {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0f;
+            }
+        }
+    }
+
+    public void PointerEnter() {
+        gazed = true;
+    }
+
+    public void PointerExit() {
+        gazed = false;
+    }
+
+    public void PointerDown() {
+
+    }
+}
