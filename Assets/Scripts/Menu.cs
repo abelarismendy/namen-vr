49c9efed4f367c8ac72ba59354ec680f92bb7b92
mar-cas3 <61592394+mar-cas3@users.noreply.github.com>
2020-11-29 11:28:44 -0500

    MenuPrincipalV4

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
+    public GameObject panelInfo; 
+    public Button btn; 
     public void ChangeScene(string name) {
         SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+    public void OpenInfoPanel ()
+    {
+        panelInfo.SetActive(true);
+    }
+
+    public void  CloseInfoPanel ()
+    {
+        panelInfo.SetActive(false);
     }
 }
